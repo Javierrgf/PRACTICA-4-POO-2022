3a100322a63470f9775e579562647fe2a808f30d
Javier Garcia Fernandez <70642519+i12gafej@users.noreply.github.com>
2022-12-21 18:10:26 +0100

    Create ordena2.cc

---
+++ poo/p7/ordena2.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+bool descen(int i, int j){return i > j;};
+bool ascen(int i, int j){return i < j;};
+
+int main(){
+    vector<int> v;
+    bool i = true;
+    int x, y;
+    while(i){
+        cout<<"Introduzca un numero entero"<<endl;
+        cin>>x;
+        v.push_back(x);
+        system("clear");
+        cout<<"El numero que ha introducido ha sido "<<x<<endl;
+        cout<<"Introduce otro?"<<endl;
+        cout<<"1. si"<<endl;
+        cout<<"2. no"<<endl;
+        cout<<"-->";
+        cin>>y;
+        if(y == 2)
+            i = false;
+    }
+    system("clear");
+    cout<<"----------------------VECTOR DESORDENADOR----------------------"<<endl;
+    for(int i = 0; i < static_cast<int>(v.size()); i++){
+        if(i < 10)
+            cout<<"             Posicion "<<i<<":   "<<v[i]<<endl;
+        else
+            cout<<"             Posicion "<<i<<":  "<<v[i]<<endl;
+    }
+    cout<<"----------------------------------------------------------------"<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<"En que orden desea el vector?"<<endl;
+    cout<<"1.    ASCENDENTE"<<endl;
+    cout<<"OTRO. DESCENDENTE"<<endl;
+    cout<<"-->";
+    cin>>y;
+    if(y == 1){
+        sort(v.begin(), v.end(), ascen);
+    }
+    else{
+        sort(v.begin(), v.end(), descen);
+    }
+    system("clear");
+    cout<<"----------------------VECTOR ORDENADOR----------------------"<<endl;
+    for(int i = 0; i < static_cast<int>(v.size()); i++){
+        if(i < 10)
+            cout<<"             Posicion "<<i<<":   "<<v[i]<<endl;
+        else
+            cout<<"             Posicion "<<i<<":  "<<v[i]<<endl;
+    }
+    cout<<"----------------------------------------------------------------"<<endl;
+    return 0;
+}
