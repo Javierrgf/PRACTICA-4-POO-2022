6ebf216254a1cf52a3b322188bf5435b134c1a65
Javier Garcia Fernandez <70642519+i12gafej@users.noreply.github.com>
2022-12-21 18:37:43 +0100

    Update client_main.cc

--- poo/market/marketplace/src/client/client_main.cc
+++ poo/market/marketplace/src/client/client_main.cc
@@ -9,7 +9,8 @@
 using namespace std;
 
 int main(){
-    Client CLI1("0001", "JUAN", "JOSE", "11111", "C/ ALTA", "cordoba", "ESPAÑA", 2002, 4);
+    //id, name, surname, address, town, province, country, entry_year
+    Client CLI1("0001", "JUAN", "JOSE",  "C/ ALTA","Villa del rio", "cordoba", "ESPAÑA", 2002, 4);
     cout<<CLI1.get_premium()<<endl;
     return 0;                     
 }
