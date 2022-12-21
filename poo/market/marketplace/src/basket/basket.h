18b907bf3595af428e7a5d1cb3eff9138074d560
Javier Garcia Fernandez <70642519+i12gafej@users.noreply.github.com>
2022-12-21 18:31:32 +0100

    Update basket.h

--- poo/market/marketplace/src/basket/basket.h
+++ poo/market/marketplace/src/basket/basket.h
@@ -1,3 +1,4 @@
+//basket header
 #include <string>
 #include <stdlib.h>
 #include <iostream>
@@ -14,20 +15,24 @@ using namespace std;
 
 class Basket{
     private:
+    //private variables
         list<Product> product_list_;
         float total_;
         
         map<string, int>product_quantity_;
-
+    
     public:
+        //constructor
         Basket(){total_ = 0.0;};
         
-        
+        //methods
+        //observer
         inline float get_total(){return total_;};
+        //modifier
         inline float set_total(float total){return total_ = total;};
-        vector<string> get_ids();
-        vector<int> get_qs();
-        int get_size();
+        vector<string> get_ids(); //returns product id's vector
+        vector<int> get_qs(); //returns product quantities's vector
+        int get_size();//returns basket size
         
         void add_product(Product p);
         bool delete_product(Product p);        
